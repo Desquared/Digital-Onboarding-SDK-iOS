a31977b26c8f92a98a1e6e939b5a8a10aa175f80
Anka <runner@sjc20-cw712-c12a5c54-82c6-4602-8b57-f207a3bb4c5c-86CFC5B0EE12.local>
2026-03-13 12:53:57 +0000

    Release 0.0.52
    Automated release from main repository

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DIVEsignSDK",
-            url: "https://github.com/Desquared/Digital-Onboarding-SDK-iOS/releases/download/0.0.51/DIVEsignSDK-0.0.51.xcframework.zip",
-            checksum: "b3732b16249caf20752fd7c8281c737d42c5f686cf9ed9b6c6be7b94609c9c0d"
+            url: "https://github.com/Desquared/Digital-Onboarding-SDK-iOS/releases/download/0.0.52/DIVEsignSDK-0.0.52.xcframework.zip",
+            checksum: "ce1e11faa15768a5cd8f552e1d02aaeded5e78059b75292ccfe3471076e77162"
         )
     ]
 )
